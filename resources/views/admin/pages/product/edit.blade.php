bd3fb90c5dc50552ada4fb0fceda1caaad316499
Ito <waskitodamar17@gmail.com>
2024-02-05 09:18:59 +0700

    - link ke detail product di page kategori (user)

--- resources/views/admin/pages/product/edit.blade.php
+++ resources/views/admin/pages/product/edit.blade.php
@@ -8,7 +8,7 @@
     @endpush
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Tambah Produk</h1>
+        <h1 class="h3 mb-0 text-gray-800">Edit Produk</h1>
     </div>
 
     @if (session()->has('success'))
