f6e26dc9b60e898bda924ec50f7b29d998304b5d
Ito <waskitodamar17@gmail.com>
2024-02-06 16:10:10 +0700

    - tambahan page profile (user) - tambahan page change password (user)

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -3,15 +3,48 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdatePasswordRequest;
+use App\Http\Requests\UpdateProfileRequest;
+use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
     public function show()
     {
+        $user = User::find(Auth::user()->id);
+        return view('user.pages.profile.detail')
+            ->with(compact('user'));
     }
 
-    public function update()
+    public function update(UpdateProfileRequest $request)
     {
+        UserService::userDetail(Auth::user()->id)->update($request);
+
+        return redirect()->back()->with('success', 'Profile berhasil dirubah.');
+    }
+
+    public function changePassword()
+    {
+        $user = User::find(Auth::user()->id);
+        return view('user.pages.profile.password-edit-form')
+            ->with(compact('user'));
+    }
+
+    public function updatePassword(UpdatePasswordRequest $request)
+    {
+        $validated = $request->validated();
+        // dd($validated);
+        $user = UserService::userDetail(Auth::user()->id);
+
+        if (Hash::check($validated['old_password'], $user->fetch()->password)) {
+            $user->update($request);
+            return redirect()->to(route('user.profile'))->with('success', 'Password berhasil dirubah.');
+        }
+
+        return redirect()->back()->with('error', 'Password yang anda masukkan salah.');
     }
 }
